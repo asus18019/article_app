bb702ddc40313570bfd5cd197dab23afce3b33d8
Viktor Volokhan <voloxanvitya@gmail.com>
2023-01-20 21:54:22 +0200

    added article page markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import React, { FC } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Box } from '@mui/material';
 
 import './App.css';
 import Home from './pages/Home';
 import Article from './pages/Article';
-import Header from './components/Header';
 
 const App: FC = () => {
 	return (
-		<Box sx={{ p: 6 }}>
-			<Header/>
 			<Routes>
 				<Route index element={ <Home/> }/>
-				<Route path='/article' element={ <Article/> }/>
+				<Route path='/article/:articleId' element={ <Article/> }/>
 			</Routes>
-		</Box>
 	);
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,9 @@
 import React, { FC, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { Container, Typography, Divider, Grid, CircularProgress } from '@mui/material';
+import { Box, Container, Typography, Divider, Grid, CircularProgress } from '@mui/material';
 
 import Post from '../components/Post';
+import Header from '../components/Header';
 import { fetchPosts, selectPosts } from '../redux/post/slice';
 import { Status } from '../redux/post/types';
 import { useAppDispatch } from '../redux/store';
@@ -16,28 +17,31 @@ const Home: FC = () => {
 	}, []);
 
 	return (
-		<Container disableGutters>
-			<Typography variant="h4" component="h2" style={ { fontSize: 16, fontWeight: 600, fontFamily: 'Montserrat', marginTop: 40 } }>
-				Results: { fetchedData.posts.length }
-			</Typography>
-			<Divider style={ { marginTop: 5 } }/>
-			<Grid container spacing={ 6 } style={ { marginTop: 0 } }>
-				{ fetchedData.status === Status.LOADING ? (
-					<Grid item xs={ 12 } sx={{ display: 'flex', justifyContent: 'center' }}>
-						<CircularProgress />
-					</Grid>
-				) : (
-					fetchedData.posts?.map(post => {
-						return (
-							<Grid item xs={ 4 } key={ post.id }>
-								<Post post={ post }/>
-							</Grid>
-						);
-					})
-				)
-				}
-			</Grid>
-		</Container>
+		<Box sx={ { p: 6 } }>
+			<Header/>
+			<Container disableGutters>
+				<Typography variant="h4" component="h2" style={ { fontSize: 16, fontWeight: 600, fontFamily: 'Montserrat', marginTop: 40 } }>
+					Results: { fetchedData.posts.length }
+				</Typography>
+				<Divider style={ { marginTop: 5 } }/>
+				<Grid container spacing={ 6 } style={ { marginTop: 0 } }>
+					{ fetchedData.status === Status.LOADING ? (
+						<Grid item xs={ 12 } sx={ { display: 'flex', justifyContent: 'center' } }>
+							<CircularProgress/>
+						</Grid>
+					) : (
+						fetchedData.posts?.map(post => {
+							return (
+								<Grid item xs={ 4 } key={ post.id }>
+									<Post post={ post }/>
+								</Grid>
+							);
+						})
+					)
+					}
+				</Grid>
+			</Container>
+		</Box>
 	);
 };
 
